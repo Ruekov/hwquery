7cec4634982a5cb49536eda1683e757d6c7cbe86
guillem <guillemrueda@gmail.com>
2013-10-03 20:51:21 +0200

    update

--- test_cache_phi.c
+++ test_cache_phi.c
@@ -9,12 +9,28 @@
 #include <malloc.h>
 #include <papif.h>
 
-#define samples 10000
-#define CLEAN_CACHE {\
- for (i = 0; i < cache_size; i++){ \
-			x += mem[(extra_value+1+i)*num_sets*block_size]; \
-			}\
-			}
+#define samples 100000
+			
+#define CLEAN_CACHE_N {\
+		for(i=0;i < extra_value-1;i++){ \
+		mem[i*num_sets*block_size] = extra_value+1+i; \
+		}\
+		}
+
+#define PRINT_CACHE_N {\
+		printf("Vector phi_clear: "); \
+		for(i=0;i < extra_value-1;i++){ \
+		printf(" %d ",mem[i*num_sets*block_size]); \
+		}\
+		printf("\n"); \
+		}
+		
+#define SHOW_PHI {\
+		for(i=0; i < size; i++){ \
+		printf(" %d ",phi[i]); \
+		}\
+		printf(" \n"); \
+		}
 
 // global variables
 
@@ -23,12 +39,12 @@ int block_size;
 int cache_size;
 int num_sets;
 int extra_value;
+unsigned char *mem = NULL;
 int memory_size;
-char *mem;
 
 // functions
 
-void hwquery(int addr[], int hit[], size_t size, int assoc, int blocksz, int cacheSize, int memSize);
+void hwquery(int addr[], int hit[], size_t size, int assoc, int blocksz, int cacheSize, int memorySize);
 int test_access(int phi[], size_t size, int n_samples);
 	
 int main(int argc, char **argv)
@@ -44,19 +60,20 @@ int main(int argc, char **argv)
 		printf(" Vector query: %i - Hit or Miss: %i \n",access_test[i],hits[i]);
 		
 		}
+		
 	
 	
 	return 0;
 }
 
-void hwquery(int addr[], int hit[], size_t size, int assoc, int blocksz, int cacheSize, int memSize){
+void hwquery(int addr[], int hit[], size_t size, int assoc, int blocksz, int cacheSize, int memorySize){
 	
-	int i, step;
+	int i, step, memory_size;
 	
 	associativity = assoc;
 	block_size = blocksz;
 	cache_size = cacheSize;
-	memory_size = memSize;
+	memory_size = memorySize;
 	
 	int highest_value = 0;
 	
@@ -68,26 +85,21 @@ void hwquery(int addr[], int hit[], size_t size, int assoc, int blocksz, int cac
 	
 	extra_value = highest_value + 1;
 	
+
 	//********************************************************
 	//	setting mem for acces to the cache
 	//********************************************************	
 	
-	num_sets = cache_size/(block_size*associativity);
-	
-	mem = malloc((extra_value+associativity+1)*block_size*num_sets); //sizeof(*mem)
+	num_sets = cache_size/(block_size*associativity); 
+
+	mem = malloc(memory_size); //sizeof(*mem) //(extra_value+associativity+1)*block_size*num_sets;
 	
 	if (NULL == mem) {
 		printf("Error with malloc");
 		exit(0);
     }
-	
-	
-	for(i=0;i < memory_size;i++){
-		
-		mem[i] = i;
-		
-		}
-	
+    
+    CLEAN_CACHE_N;
 	
 	// first element is always a miss
 	
@@ -136,9 +148,17 @@ void hwquery(int addr[], int hit[], size_t size, int assoc, int blocksz, int cac
 		int rphi_hit = 0; 
 		int rphi_miss = 0;
 		int rphi_test = 0;
-
+		
+		printf("TEST Phi_HIT (step: %d): ",step);
+		
 		rphi_hit  = test_access(phi_hit, step, samples);
+		
+		printf("TEST Phi_MISS (step: %d): ",step);
+		
 		rphi_miss = test_access(phi_miss, step, samples);
+		
+		printf("TEST Phi_TEST (step: %d): ",step);
+		
 		rphi_test = test_access(phi_test, step, samples);
 			
 		//********************************************************
@@ -148,16 +168,18 @@ void hwquery(int addr[], int hit[], size_t size, int assoc, int blocksz, int cac
 		if(abs(rphi_hit-rphi_test) < abs(rphi_miss-rphi_test)){
 			
 			hit[step-1] = 1;
-			printf("HIT: Hit %i, Miss %i, Test %i Prom: %i < %i \n",rphi_hit, rphi_miss, rphi_test,abs(rphi_hit-rphi_test),abs(rphi_miss-rphi_test));
+			printf("%i => HIT: Hit %i, Miss %i, Test %i Prom: %i < %i \n",step,rphi_hit, rphi_miss, rphi_test,abs(rphi_hit-rphi_test),abs(rphi_miss-rphi_test));
 			
 			}else{
 				
 			hit[step-1] = 0;	
-			printf("MISS: Hit %i, Miss %i, Test %i Prom: %i > %i \n",rphi_hit, rphi_miss, rphi_test,abs(rphi_hit-rphi_test),abs(rphi_miss-rphi_test));
+			printf("%i => MISS: Hit %i, Miss %i, Test %i Prom: %i > %i \n",step,rphi_hit, rphi_miss, rphi_test,abs(rphi_hit-rphi_test),abs(rphi_miss-rphi_test));
 	
 				}
 	
 	}
+	
+	PRINT_CACHE_N;
 	
 	free(mem);
 	mem = NULL;
@@ -169,31 +191,31 @@ int test_access(int phi[], size_t size, int n_samples){
 	initialize_papi(PAPI_L1_DCM);
 	
 	register int num_miss = 0;
-	register char x;
+	register int x = 0;
 	register int i; 
 	register int j;
 	
-	
-	// start miss counter
-	
-	CLEAN_CACHE;
-	start_papi();
+	SHOW_PHI;
 
 	for ( j = 0; j < n_samples; j++){
 		
-		CLEAN_CACHE;
+		CLEAN_CACHE_N;
+
+		/* BEGIN CRITICAL LOOP */
+		
+		start_papi();	// start miss counter
 		
 		for (i = 0; i < size; i++){
-			x += mem[phi[i]*num_sets*block_size];
+			x += mem[(phi[i])*num_sets*block_size];
 			}
-			
+		num_miss = num_miss + stop_papi(); 	// stop miss counter
+		
+		/* END CRITICAL LOOP */
 		
 	}		
 	
-	// stop miss counter
-	num_miss = stop_papi();
-	printf("%d",x);
-	
+	printf(" ACCESS X: %d  NUM MISS: %d \n",x, num_miss);
+
 	return num_miss;
 	
 	}
